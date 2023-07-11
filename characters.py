c08049774673c7d47a0c9992ce8a7f978890c7d4
Sefora Rodriguez <seforarodriguez13@gmail.com>
2023-07-11 15:16:47 -0400

    first get function

---
+++ characters.py
@@ -0,0 +1,22 @@
+characters = [
+    {
+        "id": 1,
+        "name": "Miranda"
+    },
+    {
+        "id": 2,
+        "name": "Carrie"   
+    },
+    {
+        "id": 3,
+        "name": "Samantha"
+    },
+    {
+        "id": 4,
+        "name": "Charlotte"
+    }
+]
+
+
+def get_all_characters():
+    return characters
